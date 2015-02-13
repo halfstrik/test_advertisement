7cce14f50026b25ce9d679359ee5a99775de7cc0
strikki <halfstrik at gmail dot com>
2015-02-13 12:33:32 +0300

    Added view for listing existing text couples. Added base.html - base template for texts app.

--- texts/urls.py
+++ texts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+                       url(r'^$', 'texts.views.list_text_couples', name='list_text_couples'),
                        url(r'^add_text_couple/$', 'texts.views.add_text_couple', name='add_text_couple'), )
--- texts/views.py
+++ texts/views.py
@@ -1,8 +1,14 @@
+from django.core.urlresolvers import reverse
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from texts.forms import TextCoupleForm
 from texts.models import TextCouple
+
+
+def list_text_couples(request):
+    return render(request, 'texts/list_text_couples.html', {'text_couples': list(TextCouple.objects.all())})
 
 
 def add_text_couple(request):
@@ -12,7 +18,7 @@ def add_text_couple(request):
             short_cleaned = form.cleaned_data['short']
             long_cleaned = form.cleaned_data['long']
             TextCouple.objects.create(long=long_cleaned, short=short_cleaned)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(reverse('texts:list_text_couples'))
     else:
         form = TextCoupleForm()
     return render(request, 'texts/add_text_couple.html', {'form': form})
