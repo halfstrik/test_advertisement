2b8e0fda5ce37bab80ffd69e4fd95c32a3eece69
santiagarka <santiagarka1@gmail.com>
2015-02-16 11:04:25 +0300

    Added 8 new tests

--- texts/tests.py
+++ texts/tests.py
@@ -77,6 +77,97 @@ class ViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['text_couples']), 0)
 
+    def test_list_text_couples_http_get_must_contain_link_to_separate_text_couples(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        response = client.get(reverse('texts:list_text_couples'))
+        self.assertContains(response, '<a href="%s">%s %s</a>' % (reverse('texts:view_text_couple',
+                                                                          args=[id_text_couple]), short, long))
+
+    def test_text_couple_http_get(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        response = client.get(reverse('texts:view_text_couple', args=[id_text_couple]))
+        self.assertContains(response, '%s %s' % (short, long))
+
+    def test_del_text_couple_http_get(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        client.get(reverse('texts:del_text_couple', args=[id_text_couple]))
+        response = client.get(reverse('texts:list_text_couples'))
+        self.assertNotContains(response, short)
+        self.assertNotContains(response, long)
+
+    def test_del_text_couple_http_post_redirect_to_list_text_couple(self):
+        short = 'short'
+        long = 'long'
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        response = client.get(reverse('texts:del_text_couple', args=[id_text_couple]))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.endswith(reverse('texts:list_text_couples')))
+
+    def test_change_text_couple_http_post(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        change_short = ''.join(random.sample('abcdefjk', 6))
+        change_long = ''.join(random.sample('zxytrewq', 7))
+        client = Client()
+        client.post(reverse('texts:change_text_couple', args=[id_text_couple]),
+                    {'short': change_short, 'long': change_long})
+        response = client.get(reverse('texts:list_text_couples'))
+        self.assertNotContains(response, short)
+        self.assertNotContains(response, long)
+        self.assertContains(response, change_short)
+        self.assertContains(response, change_long)
+
+    def test_change_text_couple_http_post_redirect_to_view_text_couple(self):
+        short = 'short'
+        long = 'long'
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        change_short = 'new short'
+        change_long = 'new long'
+        client = Client()
+        response = client.post(reverse('texts:change_text_couple', args=[id_text_couple]),
+                               {'short': change_short, 'long': change_long})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.endswith(reverse('texts:list_text_couples')))
+
+    def test_text_couple_http_get_must_contain_link_to_delete_text_couple(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        response = client.get(reverse('texts:view_text_couple', args=[id_text_couple]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<a href="%s">Delete text couple</a>' % reverse('texts:del_text_couple',
+                                                                                      args=[id_text_couple]))
+
+    def test_text_couple_http_get_must_contain_link_to_change_text_couple(self):
+        short = ''.join(random.sample('abcdefjk', 6))
+        long = ''.join(random.sample('zxytrewq', 7))
+        id_text_couple = random.randint(1, 10)
+        TextCouple.objects.create(id=id_text_couple, short=short, long=long)
+        client = Client()
+        response = client.get(reverse('texts:view_text_couple', args=[id_text_couple]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<a href="%s">Change text couple</a>' % reverse('texts:change_text_couple',
+                                                                                      args=[id_text_couple]))
+
 
 class FormTests(TestCase):
     def test_TextCoupleForm_with_short_text_len_equals_31(self):
