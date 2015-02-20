4907a6ef278278b3249d8b1c22c9386d8aaeb6b5
santiagarka <santiagarka1@gmail.com>
2015-02-20 14:28:13 +0300

    Changed  templates, added css

--- texts/urls.py
+++ texts/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
                        url(r'^add_text_couple/$', 'texts.views.add_text_couple', name='add_text_couple'),
@@ -8,4 +10,5 @@ urlpatterns = patterns('',
                        url(r'^del_text_couple/(?P<text_couple_id>\d+)/$', 'texts.views.delete_text_couple',
                            name='del_text_couple'),
                        url(r'^change_text_couple/(?P<text_couple_id>\d+)/$', 'texts.views.change_text_couple',
-                           name='change_text_couple'), )
+                           name='change_text_couple'),
+                       ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
